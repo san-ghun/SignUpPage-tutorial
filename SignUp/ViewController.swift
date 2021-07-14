0cb0e3d45e918f1d5a3138fbd2fe7a462f7e92ad
Sanghun <po4865+github_sanghun@gmail.com>
2021-07-14 15:37:16 +0900

    practiced state change of view in ViewController

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -12,8 +12,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("A view of ViewController did loaded on the memory")
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("A view of ViewController will appear on the display")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("A view of ViewController did appear on the display")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("A view of ViewController will disappear from the display")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("A view of ViewController did disappear fron the display")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("A view of ViewController will layout subviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("A view of ViewController did layout subviews")
+    }
+    
 }
 
